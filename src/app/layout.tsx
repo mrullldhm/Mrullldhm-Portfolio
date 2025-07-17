b0651038ef483d928b05ee2aaf884b2d6d8e6c74
mrullldhm <mrullldhm94@gmail.com>
2025-07-18 05:32:00 +0800

    Setup the base and header

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Amirul Adham | Portfolio",
+  description: "Amirul Adham is a full-stack developer with ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased relative h-[5000px]`}
+      >
+        <div className="bg-[#333446] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#7F8CAA] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/lib/data.ts
@@ -0,0 +1,129 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+// import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import project1 from "@/assets/work-1.png";
+import project2 from "@/assets/work-2.png";
+import project3 from "@/assets/work-3.png";
+import project4 from "@/assets/work-4.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Assistant Manager",
+    location: "18 Tai Seng, Singapore",
+    description:
+      "Assisted in daily operations of the F&B outlet, managed staff schedules, ensured high service standards, and supported inventory and cost control to enhance customer satisfaction and efficiency.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2021 - 2023",
+  },
+  {
+    title: "Management Trainee",
+    location: "Queenstown, Singapore",
+    description:
+      "Underwent structured training in operations, staff supervision, and quality control to prepare for leadership roles in the cleaning service industry. ",
+    icon: React.createElement(CgWorkAlt),
+    date: "2020 - 2020",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Moresume - AI Powered Resume Builder",
+    description:
+      "Built a full-stack resume builder with AI summary generation, drag-and-drop forms, secure authentication, and PDF export. Integrated OpenAI, Stripe, and Clerk for a seamless user experience.",
+    tags: [
+      "React",
+      "Next.js",
+      "Typescript",
+      "Tailwind",
+      "PostgreSQL",
+      "Prisma",
+      "Stripe",
+      "Clerk",
+    ],
+    imageUrl: project1,
+  },
+  {
+    title: "Image Search App – Powered by Pexels",
+    description:
+      "Created a responsive image search app using the Pexels API with JavaScript. Features include search, pagination, and modal image previews with full mobile support.",
+    tags: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+    imageUrl: project2,
+  },
+  {
+    title: "Financial Knowledge Quiz Game",
+    description:
+      "Developed an interactive quiz game to test financial literacy. Includes dynamic questions, real-time scoring, and a mobile-friendly design.",
+    tags: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+    imageUrl: project3,
+  },
+
+  {
+    title: "Calculator Application",
+    description:
+      "Built a fully functional calculator with support for basic and advanced operations. Includes keyboard support and responsive layout for various devices",
+    tags: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+    imageUrl: project4,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "R",
+  "React",
+  "Next.js",
+  "Vite",
+  "Node.js",
+  "Express.js",
+  "SQL",
+  "Bash",
+  "Git",
+  "GitHub",
+  "Tailwind",
+  "Bootstrap",
+  "Shadcn",
+  "Prisma",
+  "PostgreSQL",
+  "Postman",
+  "Insomnia",
+  "Tableplus",
+  "Stripe",
+  "Clerk",
+  "DigitalOceabn",
+  "Vercel",
+  "Cloudflare",
+  "Render",
+  "Figma",
+  "Ubuntu",
+  "Framer Motion",
+] as const;
