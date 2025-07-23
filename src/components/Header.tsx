3fd7e75e0a7de8ec90c84a5345f25b5519e9a674
mrullldhm <mrullldhm94@gmail.com>
2025-07-23 22:21:44 +0800

    Adjust header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,17 +8,18 @@ import clsx from "clsx";
 import { useActiveSectionContext } from "@/context/ActiveSectionContext";
 
 export default function Header() {
-  const { activeSection, setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+  const { activeSection, setActiveSection, setTimeOfLastClick } =
+    useActiveSectionContext();
 
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full backdrop-blur-xs sm:top-6 sm:h-[3.25rem] sm:w-[36rem]"
+        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full sm:top-6 sm:h-[3.25rem] sm:w-[36rem]"
         initial={{ y: -100, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
       ></motion.div>
 
-      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2  sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+      <nav className="bg- border backdrop-blur-3xl rounded-full flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2  sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link, index) => (
             <motion.li
@@ -43,15 +44,9 @@ export default function Header() {
                 {link.name}
 
                 {link.name === activeSection && (
-                  <motion.span
-                    className="border-y border-black absolute inset-0 -z-10"
-                    layoutId="activeSection"
-                    transition={{
-                      type: "spring",
-                      stiffness: 380,
-                      damping: 30,
-                    }}
-                  ></motion.span>
+                  <span
+                    className="bg-black rounded-full opacity-10 absolute inset-0 -z-10"
+                  ></span>
                 )}
               </Link>
             </motion.li>
